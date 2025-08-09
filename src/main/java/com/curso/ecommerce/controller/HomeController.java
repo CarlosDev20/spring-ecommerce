d0f0b34a9185d041868c254426302b68078ff3b2
CarlosDev20 <ca179834@gmail.com>
2025-08-09 15:54:38 -0500

    funcionalidad de poder ver el carrito agregada

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -70,7 +70,13 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio() * cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar que el producto no se añada 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+        if(!ingresado){
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
         orden.setTotal(sumaTotal);
@@ -79,8 +85,8 @@ public class HomeController {
         model.addAttribute("orden", orden);
 
         return "usuario/carrito";
-    }    
-
+    }   
+    
     //quitar un producto del carrito
 
     @GetMapping("/delete/cart/{id}")
@@ -107,4 +113,12 @@ public class HomeController {
 
         return "usuario/carrito";
     }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+        return "/usuario/carrito";
+    }
+
 }
